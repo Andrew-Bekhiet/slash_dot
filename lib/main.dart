a0d50ea571df9e296d68d82c4bc5280532a492af
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2024-03-09 20:33:43 +0200

    feat: add navigation service
    chore(dependencies): add riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:slash_dot/services/navigation.dart';
 
-void main() {
-  runApp(const MainApp());
+ProviderContainer? _globalProviderContainer;
+
+ProviderContainer get globalProviderContainer =>
+    _globalProviderContainer ??= ProviderContainer();
+
+@visibleForTesting
+set globalProviderContainer(ProviderContainer container) {
+  _globalProviderContainer = container;
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+void main() {
+  runApp(
+    ProviderScope(
+      parent: globalProviderContainer,
+      child: const SlashDotApp(),
+    ),
+  );
+}
+
+class SlashDotApp extends StatelessWidget {
+  static final ThemeData defaultDarkTheme = ThemeData.from(
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: const Color(0xffb8ee2e),
+      brightness: Brightness.dark,
+    ),
+    useMaterial3: true,
+  );
+
+  const SlashDotApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      darkTheme: defaultDarkTheme,
+      themeMode: ThemeMode.dark,
+      routes: NavigationService.instance.routes,
     );
   }
 }
---
+++ lib/services/navigation.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:riverpod/riverpod.dart';
+import 'package:slash_dot/main.dart';
+
+final Provider<NavigationService> navigationServiceProvider =
+    Provider((ref) => const NavigationService._());
+
+class NavigationService {
+  static NavigationService get instance =>
+      globalProviderContainer.read(navigationServiceProvider);
+
+  const NavigationService._();
+
+  Map<String, Widget Function(BuildContext)> get routes => {
+        // '/': _allProductsPageBuilder,
+        // '/product': _productDetailsPageBuilder,
+      };
+
+  // Widget _allProductsPageBuilder(BuildContext context) => AllProductsPage();
+
+  // Widget _productDetailsPageBuilder(BuildContext context) =>
+  //     ProductDetailsPage();
+}
