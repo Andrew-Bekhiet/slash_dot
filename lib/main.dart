e5cf4a3551e85992e68c9d55462997cbcef2eaa6
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2024-03-09 20:38:10 +0200

    ui: remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,7 @@ class SlashDotApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       darkTheme: defaultDarkTheme,
       themeMode: ThemeMode.dark,
       routes: NavigationService.instance.routes,
